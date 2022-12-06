f44105218af65b044fa2d63134857d4af6a42525
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-06 21:45:56 +0000

    added a stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Stage } from '@react-three/drei'
+import { useState } from 'react';
 
 function App() {
+  const [shouldRotate, setShouldRotate] = useState<boolean>(false);
 
   const myColors = [
     "#cc0c39",
@@ -10,25 +12,39 @@ function App() {
     "#f8fcc1",
     "#1693a7"
   ]
+  const handleToggleRotate = () => {
+    setShouldRotate((prev) => !prev);
+  }
 
   return (
-    <div id="canvas-container">
-      <Canvas>
-        <OrbitControls />
-        <Present color1={myColors[0]} color2={myColors[1]} size={1} />
-        <group position={[1, 5, 1]}>
-          <Present color1={myColors[2]} color2={myColors[3]} size={0.5} />
-        </group>
-        <mesh position-y={-3}>
-          <boxGeometry args={[1000, 0.01, 1000]} />
-          <meshStandardMaterial color={myColors[3]} />
-
-        </mesh>
-        <ambientLight intensity={0.2} />
-        <directionalLight color="white" position={[-2, 0, 5]} />
-        <directionalLight color="grey" position={[1, 0, 5]} />
-      </Canvas>
-    </div>
+    <>
+      <div className="treeApp">
+        <button onClick={handleToggleRotate}>Rotate</button>
+      </div>
+      <div id="canvas-container">
+        <Canvas>
+          <OrbitControls autoRotate={shouldRotate} autoRotateSpeed={20} />
+          <Stage intensity={0.1}>
+            <group position={[0, -3, 0]}>
+              <group position={[-4, 0, -4]}>
+                <Present color1={myColors[0]} color2={myColors[1]} size={1} />
+              </group>
+              <group position={[4, 0, 4]}>
+                <Present color1={myColors[2]} color2={myColors[3]} size={0.5} />
+              </group>
+            </group>
+            <Tree />
+          </Stage>
+          {/* <mesh position-y={-10}>
+            <boxGeometry args={[1000, 0.01, 1000]} />
+            <meshStandardMaterial color={myColors[3]} />
+          </mesh> */}
+          {/* <ambientLight intensity={0.2} />
+          <directionalLight color="white" position={[-2, 0, 5]} />
+          <directionalLight color="grey" position={[1, 0, 5]} /> */}
+        </Canvas>
+      </div>
+    </>
   )
 }
 
@@ -39,8 +55,8 @@ interface PresentProps {
   color2: string,
   size: number
 }
-
 function Present(props: PresentProps) {
+
   return (
     <group scale={props.size}>
       <mesh position={[0, 2, 0]}>
@@ -50,6 +66,25 @@ function Present(props: PresentProps) {
       <mesh position={[0, -0.6, 0]}>
         <boxGeometry args={[5, 5, 4]} />
         <meshStandardMaterial color={props.color2} />
+      </mesh>
+    </group>
+  )
+}
+
+function Tree() {
+  return (
+    <group>
+      <mesh position-y={10}>
+        <coneGeometry args={[3, 7, 8]} />
+        <meshStandardMaterial color={"green"} />
+      </mesh>
+      <mesh position-y={5}>
+        <coneGeometry args={[5, 10, 8]} />
+        <meshStandardMaterial color={"green"} />
+      </mesh>
+      <mesh position-y={0}>
+        <cylinderGeometry args={[1, 1, 10, 8]} />
+        <meshStandardMaterial color={"brown"} />
       </mesh>
     </group>
   )
