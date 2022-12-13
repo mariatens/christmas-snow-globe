ab9d96e3b8f1b86281a5ecb5ba7a7e10e9d971ee
Maria <mtens17@gmail.com>
2022-12-13 20:29:02 +0000

    Add icon

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,24 @@ function App() {
           {/* https://www.dafont.com/home-christmas.font */}
           {/* https://github.com/pmndrs/drei#text3d */}
           {/* http://gero3.github.io/facetype.js/ */}
-          <Center>
-          <Text3D size={2} font={"/christmas-font.json"}> 
-            MERRY CHRISTMAS
-            <meshStandardMaterial color ={"red"}/>
-          </Text3D>
-          </Center>
+          <Float
+            speed={1} // Animation speed, defaults to 1
+            rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+            floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            floatingRange={[1, 10]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+          >
+            <Center>
+              <Text3D size={2} font={"/christmas-font.json"}>
+                MERRY 
+                <meshStandardMaterial color={"red"} />
+              </Text3D>
+              </Center>
+              <Text3D position={[6,0,0]}size={2} font={"/christmas-font.json"}>
+                CHRISTMAS
+                <meshStandardMaterial color={"green"} />
+              </Text3D>
+            
+          </Float>
           <OrbitControls autoRotate={shouldRotate} autoRotateSpeed={20} />
           <Stage intensity={0.1} >
             <group position={[0, -3, 0]}>
@@ -105,16 +117,17 @@ function SnowMan(): JSX.Element {
       {/* eyes of the snowman */}
       <mesh position={[12, 7.5, 5.5]}>
         <circleGeometry args={[0.3, 8]} />
-        <meshStandardMaterial flatShading = {true}color={"blue"} />
+        <meshStandardMaterial flatShading={true} color={"blue"} />
       </mesh>
       <mesh position={[16, 7.5, 5.5]}>
         <circleGeometry args={[0.3, 8]} />
-        <meshStandardMaterial flatShading = {true}color={"blue"} />
+        <meshStandardMaterial flatShading={true} color={"blue"} />
       </mesh>
 
       {/* nose of the snowman */}
       <mesh position={[14, 6.5, 5.5]}>
-        <circleGeometry args={[0.7, 3]} /> //TODO: change to cone 
+        {/* <circleGeometry args={[0.7, 3]} /> //TODO: change to cone */}
+        <coneGeometry args={[]} />
         <meshStandardMaterial color={"orange"} />
       </mesh>
       {/*  buttons of the snowman */}
@@ -187,15 +200,15 @@ function Tree() {
     <group>
       <mesh position-y={10}>
         <coneGeometry args={[3, 7, 8]} />
-        <meshStandardMaterial flatShading = {true} color={"green"} />
+        <meshStandardMaterial flatShading={true} color={"green"} />
       </mesh>
       <mesh position-y={5}>
         <coneGeometry args={[5, 10, 8]} />
-        <meshStandardMaterial flatShading = {true} color={"green"} />
+        <meshStandardMaterial flatShading={true} color={"green"} />
       </mesh>
       <mesh position-y={0}>
         <cylinderGeometry args={[1, 1, 10, 8]} />
-        <meshStandardMaterial flatShading = {true} color={"brown"} />
+        <meshStandardMaterial flatShading={true} color={"brown"} />
       </mesh>
     </group>
   )
